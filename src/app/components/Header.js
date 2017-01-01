29dde58bfffc0c797c9ecfb72b687b4125c53a1b
Maria Shodunke (The Boss Lady) <mariashow2001@gmail.com>
2017-01-01 21:35:49 +0000

    Parent child

--- src/app/components/Header.js
+++ src/app/components/Header.js
@@ -1,19 +1,17 @@
 import React from 'react';
 
-export class Header extends React.Component {
-	render() {
-		return (
+export const Header = (props) => {
+	return (
       <nav className="navbar navbar-default">
         <div className=".container">
           <div className="navbar-header">
 					
             <ul className="nav navbar-nav">
-              <li><a href="#">Home</a></li>
+              <li><a href="#">{props.homeLink}</a></li>
             </ul>
 						
 					</div>
         </div>
       </nav>
-		);
-	}
-}
+	);
+};
--- src/app/components/Home.js
+++ src/app/components/Home.js
@@ -1,27 +1,43 @@
 import React from 'react';
 
 export class Home extends React.Component {
-	constructor(props) {
+	constructor (props) {
 		super();
 		this.state = {
 			age: props.initialAge,
-			status: 0
+			status: 0,
+			homeLink: 'Changed'
 		};
+		setTimeout(() => {
+			this.setState({
+				status: 1
+			});
+		},3000);
 	}
 
-	onMakeOlder() {
+	onMakeOlder () {
 		this.setState({
 			age: this.state.age + 3
 		});
 	}
 
-	render() {
+	onChangeLink () {
+		this.props.changeLink(this.state.homeLink);
+	}
+
+	render () {
 		return (
 			<div>
 				<p>Your name is {this.props.name}, your age is {this.state.age}</p>
 				<p>Status: {this.state.status}</p>
 				<hr/>
 				<button onClick={() => this.onMakeOlder()} className="btn btn-primary">Make me older!</button>
+				
+				<hr/>
+				<button onClick={this.props.greet} className="btn btn-primary">Greet</button>
+				
+				<hr/>
+				<button onClick={(newName) => this.onChangeLink(newName)} className="btn btn-primary">Change Header Link</button>
       </div>
 		);
 	}
@@ -29,5 +45,6 @@ export class Home extends React.Component {
 
 Home.propTypes = {
 	name: React.PropTypes.string,
-	initialAge: React.PropTypes.number
+	initialAge: React.PropTypes.number,
+	greet: React.PropTypes.func
 };
