b0534fb8f227c51a888c28fa9ef69b59e6f0a452
maria-robobug <maria-robobug@users.noreply.github.com>
2017-01-01 21:52:40 +0000

    Parent child (#11)
    * Parent child

--- src/app/index.js
+++ src/app/index.js
@@ -5,17 +5,38 @@ import {Header} from './components/Header';
 import {Home} from './components/Home';
 
 class App extends React.Component {
-	render() {
+	constructor (){
+		super();
+		this.state = {
+			homeLink: 'Home'
+		};
+	}
+	onGreet () {
+		alert('Hello human!');
+	}
+	
+	onChangeLinkName (newName) {
+		this.setState({
+			homeLink: newName
+		});
+	}
+	
+	render () {
 		return (
       <div className="container">
         <div className="row">
           <div className=".col-xs-10.col-xs-offset-1">
-            <Header/>
+            <Header homeLink={this.state.homeLink}/>
           </div>
         </div>
         <div className="row">
           <div className=".col-xs-10.col-xs-offset-1">
-            <Home name={'Maria'} initialAge={29}/>
+            <Home 
+								name={'Maria'}
+								initialAge={29}
+								greet={this.onGreet}
+								changeLink={(newName) => this.onChangeLinkName(newName)}
+						/>
           </div>
         </div>
       </div>
